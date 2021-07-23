60d6a28dffdb5c413b2887ce37428e874876d539
Robert Engler <rengler33@gmail.com>
2021-07-22 22:00:24 -0500

    Implement bad guesses

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -31,9 +31,8 @@ defmodule Hangman.Game do
   end
 
 
-  def accept_move(game, guess, _already_guessed = true) do
+  def accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
-    |> score_guess(Enum.member?(game.letters, guess))
   end
 
   def accept_move(game, guess, _already_guessed) do
@@ -49,8 +48,15 @@ defmodule Hangman.Game do
     Map.put(game, :game_state, new_state)
   end
 
-  def score_guess(game, _not_good_guess) do
-    game
+  def score_guess(game = %{turns_left: 1}, _not_good_guess) do
+    Map.put(game, :game_state, :lost)
+  end
+
+  def score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
+    %{game |
+      game_state: :bad_guess,
+      turns_left: turns_left - 1,
+    }
   end
 
 
